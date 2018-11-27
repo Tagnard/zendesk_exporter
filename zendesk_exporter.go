118a4628eb3e102269e0e34deea2c648ddc01d4b
Emil Haugbergsmyr <emil@raeven.net>
2018-11-27 21:53:20 +0100

    Moved the creation of the zendesk client.

--- zendesk_exporter.go
+++ zendesk_exporter.go
@@ -21,8 +21,9 @@ type ZendeskCollector struct {
 	zendeskQueueTime   *prometheus.Desc
 }
 
-func newZendeskCollector() *ZendeskCollector {
+func newZendeskCollector(client *zendesk.Client) *ZendeskCollector {
 	return &ZendeskCollector{
+		client: client,
 		zendeskTicketCount: prometheus.NewDesc("zendesk_tickets_count",
 			"Zendesk ticket count", []string{"domain"}, nil,
 		),
@@ -41,20 +42,16 @@ func (collector *ZendeskCollector) Collect(ch chan<- prometheus.Metric) {
 	collector.mutex.Lock()
 	defer collector.mutex.Unlock()
 
-	client := zendesk.FromEnv(
-		zendesk.LoadConfigurationFromEnv(),
-	)
-
 	var tickets []zendesk.Ticket
 	var err error
 
 	if len(os.Getenv("ZENDESK_QUERY")) != 0 {
-		tickets, err = client.Ticket().Search(os.Getenv("ZENDESK_QUERY"))
+		tickets, err = collector.client.Ticket().Search(os.Getenv("ZENDESK_QUERY"))
 		if err != nil {
 			log.Error(err)
 		}
 	} else {
-		tickets, err = client.Ticket().GetAll()
+		tickets, err = collector.client.Ticket().GetAll()
 		if err != nil {
 			log.Error(err)
 		}
@@ -78,9 +75,14 @@ func (collector *ZendeskCollector) Collect(ch chan<- prometheus.Metric) {
 }
 
 func main() {
+	// Create a new instance of the zendesk client
+	client := zendesk.FromEnv(
+		zendesk.LoadConfigurationFromEnv(),
+	)
+
 	// Create a new instance of the ZendeskCollector and
 	// registre it with the prometheus client
-	zendeskCollector := newZendeskCollector()
+	zendeskCollector := newZendeskCollector(&client)
 	prometheus.MustRegister(zendeskCollector)
 
 	// Start the HTTP server and expose
